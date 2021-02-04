bfb9694696291dd81d41045edc0287cd88b620d0
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-02-04 21:33:28 +0530

    added home page

--- moviemix/src/App.js
+++ moviemix/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import logo from "./images/logo/MovieMix4.png";
 
 function App() {
-  return <div className="App">moviemix</div>;
+  return (
+    <>
+      <div className="App">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+    </>
+  );
 }
 
 export default App;
