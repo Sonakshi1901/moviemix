a5fba327ff3a416f5800391bd044f98c8088661a
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-07 19:22:55 +0400

    Update App.js

--- moviemix/src/App.js
+++ moviemix/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import logo from "./images/logo/MovieMix4.png";
+import Movie from "../moviesList/movie/Movie.js";
 
 function App() {
   return (
     <>
       <div className="App">
         <img className="logo" src={logo} alt="logo" />
+        <Movie/>
       </div>
     </>
   );
