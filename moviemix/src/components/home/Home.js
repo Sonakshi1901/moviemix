fac1be082f77be8f7819a92876dcb69930e5815e
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-07 19:29:13 +0400

    Update Home.js

--- moviemix/src/components/home/Home.js
+++ moviemix/src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "../../images/logo/MovieMix4.png";
 import "./Home.css";
+import Movie from "../moviesList/movie/Movie.js";
 
 const Home = () => {
   return (
     <div className="home">
       <img className="home__logo" src={logo} alt="logo" />
+      <Movie/>
     </div>
   );
 };
