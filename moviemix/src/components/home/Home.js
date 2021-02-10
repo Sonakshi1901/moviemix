7a1b496140ddde97f75303ee2488b5583199f978
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-10 23:29:13 +0400

    Update Home.js

--- moviemix/src/components/home/Home.js
+++ moviemix/src/components/home/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
-import logo from "../../images/logo/MovieMix4.png";
 import "./Home.css";
-import Movie from "../moviesList/movie/Movie.js";
+import Movie from "../moviesList/movie/Movie";
+import Button from "../utility/buttons/Button";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <img className="home__logo" src={logo} alt="logo" />
-      <Movie/>
+  return(
+    <div className = "Home">
+     <Movie/>
+     <Button name = "Show"/>
     </div>
   );
 };
