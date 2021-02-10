99e55f78d0acc1e44ebf4f428aeef8680d043aa2
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-02-11 01:27:24 +0530

    added axios

--- moviemix/src/axios/Axios.js
+++ moviemix/src/axios/Axios.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
-export default axios;
+// setting base URL to make request to the movie database
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export default instance;
