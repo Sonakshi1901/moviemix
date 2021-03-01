bcc8e3091da7d452fe5dcba84c27589c8ad74fc0
Sonakshi1901 <sonakshi1901@gmail.com>
2021-03-01 16:19:52 +0400

    Update MovieList.js

--- moviemix/src/components/moviesList/MovieList.js
+++ moviemix/src/components/moviesList/MovieList.js
@@ -7,11 +7,30 @@ const MovieList = () => {
   const { movies } = useCommand();
   console.log(movies);
 
+  movies.map((obj) => ({
+    original_title: obj.original_title,
+    original_name: obj.original_name,
+    poster_path: obj.poster_path,
+    release_date: obj.release_date,
+    first_air_date: obj.first_air_date,
+    vote_average: obj.vote_average,
+    overview: obj.overview,
+    id: obj.id
+   }));
+
   return (
    <div className="movielist">
     <div className="movielist__movies">
       {movies.map((movie) => (
-        <Movie />
+        <Movie
+         key={movie.id}
+         overview={movie.overview}
+         original_title={movie.original_title}
+         original_name={movie.original_name}
+         first_air_date={movie.first_air_date}
+         poster_path={movie.poster_path}
+         release_date={movie.release_date}
+         vote_average={movie.vote_average}/>
       ))}
     </div>
    </div>
